735b79b19b86f5347311b8d07c242623369444a0
= <vivek.yadav@sapple.co.in>
2022-10-06 12:28:11 +0530

    code commited

---
+++ lib/Utils/auth.manager.dart
@@ -0,0 +1,23 @@
+// class ApiManager {
+//   var response;
+//   Future<UserLoginResponse> login(_map) async {
+//     var url = Uri.parse(EndPoint().userLogin);
+//     response = await http.post(url, body: _map);
+//     return UserLoginResponse.fromJson(jsonDecode(response.body.toString()));
+//   }
+// }
+
+import 'dart:convert';
+
+import 'package:schoolmanagement/Utils/url.dart';
+import 'package:schoolmanagement/models/login_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiManager {
+  var response;
+  Future<UserLoginResponse> getLogin(_map) async {
+    var url = Uri.parse(EndPoint().userLogin);
+    response = await http.post(url, body: _map);
+    return UserLoginResponse.fromJson(jsonDecode(response.body.toString()));
+  }
+}
